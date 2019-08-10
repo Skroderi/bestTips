aff5ee3de866450cc3a91539d77aeaf470e6910e
Skroderi <bakaminski@wp.pl>
2019-08-11 00:33:17 +0200

    register user route

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: new Date(Date.now()).toLocaleString()
+  }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const app = express();
 
 //connectDB
 connectDB();
+//Init Middleware to allow to get the data from request body
+app.use(express.json({ extends: false }));
 
 app.get("/", (req, res) => res.send("API RUNNING"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
