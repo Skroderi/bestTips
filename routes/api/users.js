944fa1d5531121d9f4b2ba9e0d6dcfdd06f7f197
Skroderi <bakaminski@wp.pl>
2019-08-12 20:46:02 +0200

    implement jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @route POST api/users
 // @desc Register User
@@ -33,7 +35,7 @@ router.post(
     try {
       // USER REGISTRATION
 
-      // 1 See if user name or email exists
+      /////////////// 1. See if user name or email exists //////////////
       let userEmail = await User.findOne({ email });
       let userName = await User.findOne({ name });
       if (userEmail) {
@@ -45,7 +47,7 @@ router.post(
           .status(400)
           .json({ errors: [{ msg: "User already exists" }] });
       }
-      // 2 Get users gravatar
+      //////////// 2. Get users gravatar ///////////
       const avatar = gravatar.url(email, {
         s: "200",
         r: "pg",
@@ -59,14 +61,33 @@ router.post(
         password
       });
 
-      // 3 Encrypt password
+      ///////// 3. Encrypt password ////////////////
+
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send("user registered");
-      //Return jsonwebtoken
+
+      // res.send("user registered");
+
+      ////////// 4. Return jsonwebtoken //////////////
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
 
       // MSG if user registered or error if not
     } catch (err) {
--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => res.send("API RUNNING"));
 //Define Routes
 app.use("/api/users", require("./routes/api/users"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
